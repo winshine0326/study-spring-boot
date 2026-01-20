0e699a19c9626b0fe6a4d28ed371f96edc862323
winshine0326 <shane10340@gmail.com>
2026-01-20 17:28:37 +0900

    레포지토리 만들고 DB 입출력 하기

--- src/main/java/org/apple/shop/ItemController.java
+++ src/main/java/org/apple/shop/ItemController.java
@@ -1,13 +1,23 @@
 package org.apple.shop;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class ItemController {
+
+  private final ItemRepository itemRepositry;
+
   @GetMapping("/list")
   String list(Model model){
+    List<Item> result = itemRepositry.findAll();
+    System.out.println(result.get(0).title);
+
     model.addAttribute("name", "홍길동");
     return "list.html";
   }
