7125c0009c49884a46c819875433eb2d9c0015ff
winshine0326 <shane10340@gmail.com>
2026-01-21 15:18:28 +0900

    navbar 만듬

--- src/main/java/org/apple/shop/ItemController.java
+++ src/main/java/org/apple/shop/ItemController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -49,4 +50,9 @@ public class ItemController {
     itemRepository.save(item);
     return "redirect:/list";
   }
+//  @PostMapping("/add") //참고
+//  String addPost(@ModelAttribute Item item){
+//    itemRepository.save(item);
+//    return "redirect:/list";
+//  }
 }
