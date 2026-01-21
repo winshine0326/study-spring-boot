1a5ffe0cec32effe1bae3db3084e7ae1c4cf954f
winshine0326 <shane10340@gmail.com>
2026-01-21 14:54:16 +0900

    데이터 form으로 입력 받아서 db에 저장하기

--- src/main/java/org/apple/shop/ItemController.java
+++ src/main/java/org/apple/shop/ItemController.java
@@ -4,26 +4,49 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @Controller
 @RequiredArgsConstructor
 public class ItemController {
 
-  private final ItemRepository itemRepositry;
+  private final ItemRepository itemRepository;
 
   @GetMapping("/list")
   String list(Model model){
-    List<Item> result = itemRepositry.findAll();
-    System.out.println(result.get(0).getTitle());
+    List<Item> result = itemRepository.findAll();
+//    System.out.println(result.get(0).getTitle());
     var a = new Item();
-    System.out.println(a);
+//    System.out.println(a);
 
     a.setTitle("adsf");
-    System.out.println(a);
+//    System.out.println(a);
 
     model.addAttribute("items", result);
     return "list.html";
+  }
+
+  @GetMapping("/write")
+  String write(){
+    return "write.html";
+  }
+
+  @PostMapping("/add")
+  String addPost(@RequestParam Map<String, String> formData){
+    String title = (String) formData.get("title");
+    Integer price = Integer.valueOf((String) formData.get("price"));
+
+    System.out.println(title);
+    System.out.println(price);
+    Item item = new Item();
+    item.setTitle(title);
+    item.setPrice(price);
+    itemRepository.save(item);
+    return "redirect:/list";
   }
 }
